a9fb69f4be16caf65e4acf33343ee301606d5151
wk645 <wk645@nyu.edu>
2017-06-24 09:09:59 -0400

    programming

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  newhash = {}
+  languages.each do |style, type|
+    type.each do |language, description|
+      if newhash[language] == nil
+        newhash[language] = description
+        newhash[language][:style] = [style]
+      else
+        newhash[language][:style] << style
+      end
+    end
+  end
+  newhash
 end
